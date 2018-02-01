137dbcf53ca129a2be53002c25e48eadf00bfe87
Artemije <artem.kuznecov.samara@gmail.com>
2018-02-01 10:08:31 +0400

    Создал дочерний класс но не придумал, что добавить или переопределить.

--- 1/1-4/index.php
+++ 1/1-4/index.php
@@ -1,8 +1,9 @@
-<?
+<?php
 
 require_once './lib/DogMother.class.php';
+require_once './lib/Puppy.class.php';
 
-// Регистрация новой собаки
+// --- Регистрация новой собаки: ---
 // Инициализация переменных
 $birth_day = "1.1.2008";
 $sex = "female";
@@ -24,16 +25,37 @@ echo "<br>";
 // Регистрируем новый помет
 $ledi->NewLitter("30.01.2018", "2", "3"); 
 
+// Продали одного кобеля и одну суку:
+$ledi->SoldMale(1);
+$ledi->SoldFemale(1);
+
 // Выводим объявление о помете
-echo "Ведется запись на щенков от собаки: ".$ledi->GetInfoAboutThisDog('nickname').". Дата рождения щенков: ". $ledi->GetInfoAboutThisDog('litter_date')." года.";
+echo "Ведется запись на щенков от собаки: ".$ledi->GetInfoAboutThisDog('nickname').". "."Дата рождения щенков: ". $ledi->GetInfoAboutThisDog('litter_date')." года. ";
 // Вывидим кол-во щенков
-echo "В наличии кобели: ".$ledi->GetInfoAboutThisDog('number_of_males')." и суки: ".$ledi->GetInfoAboutThisDog('number_of_females');
+echo "В наличии: кобелей ".$ledi->GetInfoAboutThisDog('number_of_males').", сук ".$ledi->GetInfoAboutThisDog('number_of_females').".";
 
-
-
-
-
-
-
-
-
+// --- Регистрируем нового щенка: ---
+$sharik = new Puppy($ledi->GetInfoAboutThisDog('litter_date'), 
+                    'male', 
+                    $ledi->GetInfoAboutThisDog('coloring'), 
+                    'Шарик', 
+                    $ledi->GetInfoAboutThisDog('prefix'), 
+                    23456);
+// Добавляем титулы
+$sharik->AddATitleToTheDog('Лучший любитель нагадить где не надо в классе юниор!');
+echo "<br>";
+echo "Окрас ".$sharik->GetInfoAboutThisDog('nickname').": ".$sharik->GetInfoAboutThisDog('coloring');
+echo "<br>";
+echo "Имя у ".$sharik->GetInfoAboutThisDog('nickname').": ".$sharik->GetInfoAboutThisDog('nickname');
+echo "<br>";
+echo "Приставка у ".$sharik->GetInfoAboutThisDog('nickname').": ".$sharik->GetInfoAboutThisDog('prefix');
+echo "<br>";
+echo "Тату у ".$sharik->GetInfoAboutThisDog('nickname').": ".$sharik->GetInfoAboutThisDog('stamp');
+echo "<br>";
+echo "Титулы у ".$sharik->GetInfoAboutThisDog('nickname').": ".$sharik->GetInfoAboutThisDog('titles');
+echo "<br>";
+echo "Дата помета у ".$sharik->GetInfoAboutThisDog('nickname').": ".$sharik->GetInfoAboutThisDog('litter_date');
+echo "<br>";
+echo "Число кобелей у ".$sharik->GetInfoAboutThisDog('nickname').": ".$sharik->GetInfoAboutThisDog('number_of_males');
+echo "<br>";
+echo "Число сук у ".$sharik->GetInfoAboutThisDog('nickname').": ".$sharik->GetInfoAboutThisDog('number_of_females');
--- 1/1-4/lib/DogMother.class.php
+++ 1/1-4/lib/DogMother.class.php
@@ -9,18 +9,19 @@ class DogMother
     private $nickname;                    // Кличка
     private $prefix;                      // Заводская приставка
     private $stamp;                       // Номер клейма    
-    // --- Наживное состояние: ---
+    // Наживное
     private $titles;                      // Титулы и награды собаки
-    // --- Состояние текущего помета: ---
+    // Текущий помет
     private $litter_date;                 // День крайнего помета
     private $number_of_males;             // Кол-во самцов в помете
     private $number_of_females;           // Кол-во самок в помете
+    // Кол-во щенков проданных и оставшихся
     private $nubmer_of_sold_males;        // Кол-во проданных самцов
     private $number_of_remaining_males;   // Кол-во оставшихся самцов
     private $nubmer_of_sold_females;      // Кол-во проданных самок
     private $number_of_remaining_females; // Кол-во оставшихся самок
     
-    // Конструктор для объявления собаки
+    // --- Конструктор для объявления собаки: ---
     function __construct($birth_day, $sex, $coloring, $nickname, $prefix, $stamp){
         $this->birth_day = $birth_day;
         $this->sex = $sex;
@@ -36,6 +37,7 @@ class DogMother
     { 
         $this->titles.=" ".$new_title;
     }
+    
     // Регистрирует новый помет
     public function NewLitter($litter_date, $number_of_males, $number_of_females)
     { 
@@ -43,25 +45,47 @@ class DogMother
         $this->number_of_males = $number_of_males;
         $this->number_of_females = $number_of_females;
     }
-
+    
+    // Обновление информации о купленных кобелях и оставшихся
+    public function SoldMale($number)
+    {
+        if ($this->number_of_males<$number){echo "<b style='font-size:1.2em'>У вас нет столько кобелей на продажу! </b>";}
+        else {$this->number_of_males -= $number;}
+    }
+    // Обновление информации о купленных суках и оставшихся
+    public function SoldFemale($number)
+    {
+        if ($this->number_of_females<$number){echo "<b style='font-size:1.2em'>У вас нет столько сук на продажу! </b>";}
+        else {$this->number_of_females -= $number;}   
+    }
+    
     // С ПОМОЩЬЮ КЕЙСОВ И СВИТЧА ПРОИЗВОДИТСЯ ВОЗВРАТ НЕОБХОДИМОЙ ИНФОРМАЦИИ
     public function GetInfoAboutThisDog($case)
     {
         switch ($case)
         {
-            case "nickname":
+            case "coloring": // Для возврта окраса
+                return $this->coloring;
+                break;
+            case "nickname": // Для возврта клички
                 return $this->nickname;
                 break;
-            case "titles":
+            case "prefix": // Для возврта приставки
+                return $this->prefix;
+                break;
+            case "stamp": // Для возврта приставки
+                return $this->stamp;
+                break;
+            case "titles": // Для возврата титулов
                 return $this->titles;
                 break;
-            case "litter_date":
+            case "litter_date": // Для возврата даты помета
                 return $this->litter_date;
                 break;
-            case "number_of_males":
+            case "number_of_males": // Для возврата кол-ва кобелей в помете
                 return $this->number_of_males;
                 break;
-            case "number_of_females":
+            case "number_of_females": // Для возврата кол-ва сук в помете
                 return $this->number_of_females;
                 break;            
         }

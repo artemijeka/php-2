16373c7365b0fe5e0834ebc596ee41325c3b5b7e
Artemije <artem.kuznecov.samara@gmail.com>
2018-02-03 21:31:41 +0400

    автозагрузку классов сделал

--- 2/index.php
+++ 2/index.php
@@ -1,8 +1,16 @@
 <?php
 
-require_once 'lib/PieceGoods.class.php';
-require_once 'lib/DigitalGoods.class.php';
+// Автозагрузка классов:
+spl_autoload_register(function($ClassName)
+{
+   include 'lib/' . $ClassName . '.class.php'; 
+});
 
-// Новый цифровой товар
-$dg = new PieceGoods;
-echo $dg -> getCost();
+// Новый штучный товар (стоимость, кол-во)
+$pg = new PieceGoods(1500, 3);
+echo "Стоимость диска с игрой: ".$pg -> getCost()." руб.<br>";
+echo "Кол-во дисков в корзине: ".$pg -> getAmount()." шт.<br>";
+echo "Финальная стоимость товаров в корзине: ".$pg -> countingTheCost()." руб.<br>";
+
+// Новый цифровой товар (стоимость, кол-во) 
+$dg = new DigitalGoods($pg->getCost()/2, 3);

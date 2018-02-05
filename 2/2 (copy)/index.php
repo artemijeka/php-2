9b9d8fda11553e0699a6cca0565808eba4760936
Artemije <artem.kuznecov.samara@gmail.com>
2018-02-05 19:31:56 +0400

    -

--- 2/2 (copy)/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once 'GetSingleton.class.php';
-
-// Запуск через синглтон трейт
-GetSingleton::getInstance()->getHello();
-
-// Почему так работает? Нормально ли это?
-GetSingleton::getHello();

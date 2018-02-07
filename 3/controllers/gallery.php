34420cafc28c5f168e8bbbec08980fa1bcd402e5
Artemije <artem.kuznecov.samara@gmail.com>
2018-02-07 22:39:17 +0400

    Галерея заработала

--- 3/controllers/gallery.php
+++ 3/controllers/gallery.php
@@ -1,14 +1,28 @@
 <?php
 
+// Объявление массива для путей к маленьким изображениям:
 $arrayOfPathToMiniImages = array();
 
+// Подключаем модель подсчета файлов:
 include_once 'models/count_files.php';
-$countFiles = count_files('data/images');
 
+// Подсчет кол-ва файлов в папке изображений:
+$countFiles = count_files('data/images/');
+// Подключаем модель изменения изображения:
 include_once 'models/resize_images.php';
+
+// В цикле обрабатываем папку с изображениями:
 for ($i=1; $i<=$countFiles; $i++){
-   resize(400, "data/images/mini/$i" . ".jpg", "data/images/$i" . ".jpg"); 
-   $arrayOfPathToMiniImages["$i"] = "data/images/mini/$i";
+    // Путь к маленькому изображению:
+    $miniImage = "data/mini_images/$i" . ".jpg";
+    // Путь к большому изображению:
+    $image = "data/images/$i" . ".jpg";
+    // Уменьшаем изображения:
+    resize(400, $miniImage, $image); 
+    // Присваиваем в массив путь к маленькому изображению:
+    $arrayOfPathToMiniImages["$i"] = $miniImage;
+    // Права на данное изображение:
+//    chmod($miniImage, 0755);
 }
 
 

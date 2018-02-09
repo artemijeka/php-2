43dfa46e076bb3e78f593bf301efd5c0b0ed9841
Artemije <artem.kuznecov.samara@gmail.com>
2018-02-09 20:10:54 +0400

    3 дз доделал

--- 3/controllers/image.php
+++ 3/controllers/image.php
@@ -1,3 +1,25 @@
 <?php
 
-include '../views/image.tmpl';
+$nameImage = $_GET['name'];
+
+// подгружаем и активируем авто-загрузчик Twig-а
+require_once 'lib/vendor/autoload.php';
+
+  try {
+  // указываем где хранятся шаблоны
+  $loader = new Twig_Loader_Filesystem('views');
+
+  // инициализируем Twig
+  $twig = new Twig_Environment($loader, array(
+  //    'cache' => 'data/cache',
+    'charset' => 'utf-8'
+  ));
+  // передаём в шаблон переменные и значения
+  // выводим сформированное содержание
+  echo $twig->render('image.tmpl', array(
+    'title' => 'Страница изображения - ДЗ по php-2 - урок 3 - задание 1',
+    'nameImage' => $nameImage
+  ));
+} catch (Exception $e) {
+  die ('ERROR: ' . $e->getMessage());
+}
--- 3/models/scan_dir_images.php
+++ 3/models/scan_dir_images.php
@@ -1,9 +1,9 @@
 <?php
+
 /**
  * @param string $dirPath - путь к диретории
  * @return array  - массив имён изображений
  */
-
 function scan_dir_images($dirPath)
 {
     $result = array();
@@ -13,10 +13,6 @@ function scan_dir_images($dirPath)
      * 
      */
     array_splice($scanDir, 0, 2);
-    
-//    echo '<pre>';
-//    var_dump($scanDir);
-//    echo '</pre>';
     
     return $scanDir;
 }

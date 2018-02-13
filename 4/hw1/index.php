7e37881136f9c775d3dd0f40cd8d9a492d16bd93
Artemije <artem.kuznecov.samara@gmail.com>
2018-02-13 22:07:40 +0400

    скачал твиг

--- 4/hw1/index.php
+++ 4/hw1/index.php
@@ -1,3 +1,6 @@
 <?php
+require_once '/vendor/autoload.php';
+Twig_Autoloader::register();
 
-
+// Коннект к базе данных
+require_once 'models/db_connect.php';
---
+++ 4/hw1/models/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+
+class ConnectToDB
+{
+    private $dbHost = 'localhost';
+    private $dbName = 'php_shop';
+    private $dbUser = 'root';
+    private $dbPass = '';
+    private $dbConnect;
+    
+    public function __construct() {
+        if(!isset($this->));
+    }
+}

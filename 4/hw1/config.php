8518fe28a1c11fabc50d94e641bd0a84c197bbb6
Artemije <artem.kuznecov.samara@gmail.com>
2018-02-14 06:06:48 +0400

    db_connect.php заполнил

---
+++ 4/hw1/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Файл конфигурации.
+ */
+
+// Базы данных.
+const DB_HOST = 'localhost';
+const DB_NAME = 'php_shop';
+const DB_USER = 'root';
+const DB_PASS = '';
--- 4/hw1/models/db_connect.php
+++ 4/hw1/models/db_connect.php
@@ -1,14 +1,53 @@
 <?php
+/**
+ * Класс устанавливает соединение с бд.
+ */
+
+include_once 'config.php';
 
 class ConnectToDB
 {
-    private $dbHost = 'localhost';
-    private $dbName = 'php_shop';
-    private $dbUser = 'root';
-    private $dbPass = '';
+    private $host = DB_HOST;
+    private $dbName = DB_NAME;
+    private $dbUser = DB_USER;
+    private $dbPass = DB_PASS;
     private $dbConnect;
+    private $limit = 0;
     
-    public function __construct() {
-        if(!isset($this->));
+    // Метод создания соединения с бд:
+    public function __construct() 
+    {
+        if (!isset($this->dbConnect)) {
+            try {
+               $connect = new PDO("mysql:host=".$this->host.";dbname=".$this->dbName, $this->dbUser, $this->dbPass); 
+               $connect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+               $this -> dbConnect = $connect;
+            } catch (PDOException $e) {
+                die('Ошибка подключения к БД: '.$e ->getMessage());
+            }
+        }
     }
+    
+    public function getRows($numberRows) 
+    {
+        static $limit;
+        if (isset($_GET['page'])) {
+            $limit = $numberRows * $_GET['page'];
+        }
+        else {
+            $limit += $numberRows;
+        }
+        
+        $sql = $this->dbConnect->prepare("SELECT brand.name, product.name, product.price FROM product LEFT JOIN brand.name USING (brand_id) LIMIT :limit";
+        $sql -> bindParam(':limit', $limit);
+        $sql -> execute();
+        
+        while ($row = $sql -> fetchObject()) {
+            $data[] = $row;
+        }
+        
+        return !empty($data)?$data:false;
+    }
+    
+    
 }

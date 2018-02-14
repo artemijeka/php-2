821bd6c61aa0bf19ffa90b6e6103fca6d02ea9bf
Artemije <artem.kuznecov.samara@gmail.com>
2018-02-14 06:54:38 +0400

    дамп бд

--- 4/hw1/models/db_connect.php
+++ 4/hw1/models/db_connect.php
@@ -40,7 +40,7 @@ class ConnectToDB
         
 //        var_dump( $this -> dbConnect );
         
-        $sql = "SELECT brand.name, product.name, product.price FROM product LEFT JOIN brand.name USING (brand_id) LIMIT $limit";
+        $sql = "SELECT brand.name as brand, product.name, product.price FROM product LEFT JOIN brand.name USING (brand_id) LIMIT $limit";
         $query = $this -> dbConnect -> query($sql);
         $query = execute();
         
